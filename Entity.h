1d512483e7b8b3df4f96ba230f0871ab8b682071
Chaotic46 <38705677+Chaotic46@users.noreply.github.com>
2018-04-25 21:17:57 -0400

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -29,7 +29,7 @@ class Playable: public Entity{
 public:
     Playable(int, int, int, int, int, int, int, int, int, char, const string&);  //initializes characters stats (HP, MP, Str (strength), Spd (speed), Intel(intelligence), Accuracy.
                                                                            // Also sets the Exp of the character along with their base inventory space
-    void Fight(Entity&);
+    bool Fight(Entity&);      //initiates a fight between player and hostile character. Returns true if player wins, false otherwise
 protected:
     Vector<Item*> Inventory(5); //heterogeneous list to point to items in characters inventory
     Item Armor, OnHand, OffHand;
